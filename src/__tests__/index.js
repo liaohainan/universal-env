6e15252c85ee835933d6c7d7a1dd670a36f468a6
balloonzzq <zqzhuang888@qq.com>
2019-05-16 15:29:37 +0800

    fix: remove babel and jest config

--- src/__tests__/index.js
+++
@@ -1,9 +0,0 @@
-import {createElement, Component} from 'rax';
-import renderer from 'rax-test-renderer';
-import Mod from '../';
-
-describe('Mod', () => {
-  it('test typeof Mod', () => {
-    expect(typeof Mod).toEqual('function');
-  });
-});
