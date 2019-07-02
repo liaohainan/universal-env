b154075030654bbd325b23478cf1850dc86cbcef
balloonzzq <zqzhuang888@qq.com>
2019-07-02 14:51:55 +0800

    chore: add mini app test

--- src/__tests__/index.mp.ts
+++ src/__tests__/index.mp.ts
@@ -1,6 +1,11 @@
 // eslint-disable-next-line
 (global as any).my = {
-  getSystemInfo: () => {}
+  getSystemInfo: () => {},
+  getSystemInfoSync: () => {
+    return {
+      platform: 'android'
+    };
+  }
 };
 
 describe('Mni app environment', (): void => {
@@ -8,5 +13,17 @@ describe('Mni app environment', (): void => {
     // eslint-disable-next-line
     const { isMiniApp } = require('../index');
     expect(isMiniApp).toBeTruthy();
+  });
+
+  it('Is android mini app environment', (): void => {
+    // eslint-disable-next-line
+    const { isAndroid } = require('../index');
+    expect(isAndroid).toBeTruthy();
+  });
+
+  it('Is not iOS mini app environment', (): void => {
+    // eslint-disable-next-line
+    const { isIOS } = require('../index');
+    expect(isIOS).toBe(false);
   });
 });
