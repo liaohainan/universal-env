96b9804e245612c60ebcdfade18a450d84a2cf81
liaohainan <liaohainan44@126.com>
2019-08-28 11:32:03 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // https://www.w3.org/TR/html5/webappapis.html#dom-navigator-appcodename
 declare const callNative: any;
 declare const WXEnvironment: any;
-declare const __fbBatchedBridgeConfig: any;
 declare const my: any;
 declare const wx: any;
 // compatibility taobao mp
@@ -9,7 +8,7 @@ const isTaoWebview = typeof __sfc__ !== 'undefined';
 const isWebPure: boolean = typeof navigator === 'object' && (navigator.appCodeName === 'Mozilla' || navigator.product === 'Gecko');
 export const isNode: boolean = typeof process !== 'undefined' && !!(process.versions && process.versions.node);
 export const isWeex: boolean = typeof callNative === 'function' || typeof WXEnvironment === 'object' && WXEnvironment.platform !== 'Web';
-export const isReactNative: boolean = typeof __fbBatchedBridgeConfig !== 'undefined';
+export const isReactNative: typeof navigator != "undefined" && navigator.product == "ReactNative";
 export const isMiniApp: boolean = typeof my === 'object' && typeof my.getSystemInfo !== 'undefined' && !isTaoWebview;
 export const isWechatApp: boolean = typeof wx === 'object' && typeof wx.getSystemInfo !== 'undefined';
 // In taobao mp'webview __sfc__ is an object
